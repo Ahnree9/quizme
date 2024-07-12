8349093d6711b99c8b27aa99b528e8d5f324398d
Ahnree9 <143423705+Ahnree9@users.noreply.github.com>
2024-07-12 12:34:31 -0500

    Added comments
    comments added

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,12 @@
+// Creating the shufffleAnswers function, which takes a question object as input and returns a shuffled array of asnwer options
 export const shuffleAnswers = (question) => {
+
+    // Creating an array of all answer options, with the correct answer first
     const unshuffledAnswers = [
         question.correctAnswer,
         ...question.incorrectAnswers,
     ];
 
-    return unshuffledAnswers.map(answer => ({sort: Math.random(), value: answer})).sort((a,b) => a.sort - b.sort).map((obj) => obj.value);
+    // Shuffling the answer options by assigning each answer option a random sort value, sorting the array based on those sort values, and then returning only the answer values in their shuffled order
+    return unshuffledAnswers.map(answer => ({ sort: Math.random(), value: answer })).sort((a, b) => a.sort - b.sort).map((obj) => obj.value);
 };
